c0fbecf7c431ddbfadea99af829778eaf7dbe412
alexey-yubi <50112636+alexey-yubi@users.noreply.github.com>
2019-06-07 11:34:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 import parser
 
 #main variables
-TOKEN = "os.environ['TELEGRAM_TOKEN']"
+TOKEN = "bot446260100:AAGH6JR31j2IMefwoW5zBph-P9zlT_lPHE4"
 bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):

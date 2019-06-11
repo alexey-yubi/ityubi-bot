2308f932fb20212ae13710f144d2b195ba99bffb
alexey-yubi <50112636+alexey-yubi@users.noreply.github.com>
2019-06-11 15:25:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,25 @@
 import telebot
 import parser
+import bs4
+import parser
 
 #main variables
 TOKEN = "bot446260100:AAGH6JR31j2IMefwoW5zBph-P9zlT_lPHE4"
 bot = telebot.TeleBot(TOKEN)
+#handlers
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
     bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду.')
+
+@bot.message_handler(content_types=['text'])
+def text_handler(message):
+    text = message.text.lower()
+    chat_id = message.chat.id
+    if text == "привет":
+        bot.send_message(chat_id, 'Привет, я бот - парсер.')
+    elif text == "как дела?":
+        bot.send_message(chat_id, 'Хорошо, а у тебя?')
+    else:
+        bot.send_message(chat_id, 'Простите, я вас не понял :(')
+
 bot.polling()

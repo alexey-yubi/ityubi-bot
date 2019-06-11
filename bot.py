ba174dc981ba13807be35ccb3df27b270eabb16a
alexey-yubi <50112636+alexey-yubi@users.noreply.github.com>
2019-06-11 15:59:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import bs4
 
 #main variables
 TOKEN = "bot446260100:AAGH6JR31j2IMefwoW5zBph-P9zlT_lPHE4"
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(token=TOKEN)
 #handlers
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
